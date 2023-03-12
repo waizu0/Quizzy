afc6330f1dad09a67adbcbb02b470026493ddc28
Lorran de Barros <lorranbarrinha@gmail.com>
2023-03-12 05:29:19 -0300

    Path bar

--- src/components/QuizArea.js
+++ src/components/QuizArea.js
@@ -3,13 +3,17 @@ import './QuizArea.css';
 import { DiJavascript1 } from 'react-icons/di'
 import { TbBrandCSharp } from 'react-icons/tb'
 import { AiOutlineClose } from 'react-icons/ai'
+import { FiPlay } from 'react-icons/fi'
+import { TiFlowSwitch } from 'react-icons/ti'
+import { BsLayoutSplit } from 'react-icons/bs'
+import { BiDotsHorizontalRounded, BiCube } from 'react-icons/bi'
 
 function QuizArea() {
   return (
     <div className='quiz-area-container'>
         
         
-        <div className="tabsManager">
+        <div className="tabs-manager">
                
         <ul className="tabs">
             <li className="tab">
@@ -19,8 +23,21 @@ function QuizArea() {
                 <p><TbBrandCSharp className='tab-icon'/>Unity.cs <AiOutlineClose className='close'/></p>
             </li>
         </ul>
+
+        <div className="right-corner">
+            <p className="btn"><FiPlay/></p>
+            <p className="btn"><TiFlowSwitch/></p>
+            <p className="btn"><BsLayoutSplit/></p>
+            <p className="btn"><BiDotsHorizontalRounded/></p>
+        </div>
         
         </div>
+
+        <div className="path-manager">
+               
+            <p>src {'>'} components {'>'} <DiJavascript1 className='js-path'/> quizzy.js {'>'} <BiCube className='cube'/> quizzy</p>
+               
+               </div>
     
     </div>
   )
